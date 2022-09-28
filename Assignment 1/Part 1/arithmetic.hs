1a88e9e519b37db27ab8c63768bf2335f7c46ab1
Snicket Fence <shchang@chapman.edu>
2022-09-27 18:24:20 -0700

    Added base files for Assignment 1

---
+++ Assignment 1/Part 1/arithmetic.hs
@@ -0,0 +1,72 @@
+-- A Virtual Machine (VM) for Arithmetic (template)
+-- Copyright: Alexander Kurz 2022
+
+-----------------------
+-- Data types of the VM
+-----------------------
+
+-- Natural numbers
+data NN = O | S NN
+  deriving (Eq,Show) -- for equality and printing
+
+-- Integers
+data II = II NN NN
+  deriving Show -- for printing
+
+-- Positive integers (to avoid dividing by 0)
+data PP = I | T PP
+
+-- Rational numbers
+data QQ =  QQ II PP
+
+------------------------
+-- Arithmetic on the  VM
+------------------------
+
+----------------
+-- PP Arithmetic
+----------------
+----------------
+-- NN Arithmetic
+----------------
+
+-- add natural numbers
+addN :: NN -> NN -> NN
+addN O m = m
+addN (S n) m = S (addN n m)
+
+-- multiply natural numbers
+multN :: NN -> NN -> NN
+multN O m = O
+multN (S n) m = addN (multN n m) m
+
+----------------
+-- II Arithmetic
+----------------
+
+
+----------------
+-- QQ Arithmetic
+----------------
+
+
+----------------
+-- Normalisation
+----------------
+
+
+----------------------------------------------------
+-- Converting between VM-numbers and Haskell-numbers
+----------------------------------------------------
+
+
+----------
+-- Testing
+----------
+main = do
+    print $ addN (S (S O)) (S O) -- S (S (S O))
+    print $ multN (S (S O)) (S (S (S O))) -- S (S (S (S (S (S O)))))
+
+
+
+
